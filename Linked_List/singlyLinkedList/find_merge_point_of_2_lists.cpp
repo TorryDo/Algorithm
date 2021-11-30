48d54a8b5cb65d4345236bd2d7ec4bf1cf8c0fc7
Trí Nguyễn <torrydo.mobile@gmail.com>
2021-11-30 16:01:17 +0700

    update

--- Linked_List/singlyLinkedList/find_merge_point_of_2_lists.cpp
+++ Linked_List/singlyLinkedList/find_merge_point_of_2_lists.cpp
@@ -21,28 +21,37 @@ public:
     }
 };
 
-// 
+// true
 // https://www.hackerrank.com/challenges/find-the-merge-point-of-two-joined-linked-lists/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
 
-// idea :  
-
+// idea :
 
 int findMergeNode(SinglyLinkedListNode *head1, SinglyLinkedListNode *head2)
 {
-    SinglyLinkedListNode *temp1 = head1;
-    SinglyLinkedListNode *temp2 = head2;
+    SinglyLinkedListNode *runner1 = head1;
+    SinglyLinkedListNode *runner2 = head2;
 
-    while(temp1 != 0){
+    for (; runner1 != NULL; runner1 = runner1->next)
+    {
+        runner2 = head2;
 
-        while(temp2 != 0){
-            if(&temp1 == &temp2){
-                return temp1->data;
-            }
-            temp2 = temp2->next;
+        for (; runner2 != NULL; runner2 = runner2->next)
+        {
+
+            if (runner1 == runner2)
+                return runner1->data;
         }
-
-        temp1 = temp1->next;
     }
 
-    return -1;
+    return 0;
+}
+
+int main()
+{
+    int i1 = 6;
+    int *p1 = &i1;
+    int *p2 = &i1;
+
+    bool isTrue = p1 == p2;
+    cout << isTrue;
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # CTDL_Algorithm
 
-I created this repository ( CTDL_Algorithm ) for training purpose only. Feel free to do anything in this repo
+>I created this repository ( CTDL_Algorithm ) for training purpose only. Feel free to do anything in this repo
